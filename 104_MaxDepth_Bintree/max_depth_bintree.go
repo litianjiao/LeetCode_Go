0646c5b1dd5d1539af61f258e63bdcd78d76de62
troylee <litianjiaod3@hotmail.com>
2018-03-05 18:27:34 +0800

    add 104

---
+++ 104_MaxDepth_Bintree/max_depth_bintree.go
@@ -0,0 +1,25 @@
+package lc0104
+
+/**
+ * Definition for a binary tree node. */
+ 
+  type TreeNode struct {
+      Val int
+      Left *TreeNode
+      Right *TreeNode
+  }
+
+ func maxDepth(root *TreeNode) int {
+    if root==nil{
+        return 0
+    }
+    return 1+max(maxDepth(root.Right),maxDepth(root.Left))
+}
+
+func max(a,b int)int{
+    if a>b{
+        return a
+    }else{
+        return b
+    }
+}
---
+++ 121_max_profit/max_profit.go
@@ -0,0 +1,9 @@
+package lc0121
+
+func maxProfit(prices []int) int {
+    return 0
+}
+
+func getTime(price1,price2 int)int{
+   
+}
