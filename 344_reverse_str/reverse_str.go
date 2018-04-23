91d11a6c4707bba06ae1862cd3439969d28f58a8
Troy Li <litianjiaod3@hotmail.com>
2018-04-23 23:58:01 +0800

    add 344

---
+++ 344_reverse_str/reverse_str.go
@@ -0,0 +1,24 @@
+package lc0344
+
+import "fmt"
+
+//之前写法
+func reverseString2(s string) string {
+	var result string
+	strlen := len(s)
+	for i := 0; i < strlen; i++ {
+		result = result + fmt.Sprintf("%c", s[strlen-1-i])
+	}
+	return result
+}
+
+//优化写法
+func reverseString(s string) string {
+	bytes := []byte(s)
+	for i, j := 0, len(bytes)-1; i < j; i, j = i+1, j-1 {
+		tmp := bytes[i]
+		bytes[i] = bytes[j]
+		bytes[j] = tmp
+	}
+	return string(bytes)
+}
